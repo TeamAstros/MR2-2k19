70ea68aaac6afad1626ca9f029c8b744d6896ec9
Aayush Bhansali <aayushbhansali692@gmail.com>
2019-04-03 04:12:01 +0530

    Hall Sensors and Motors Mismatch Removed

--- MR2_Relay/MR2_Relay.ino
+++ MR2_Relay/MR2_Relay.ino
@@ -3,14 +3,17 @@
 #define m3 10
 #define m4 11
 
-#define hall_fl 30
-#define hall_fr 32
-#define hall_bl 34
-#define hall_br 36
+#define hall_fl 24
+#define hall_fr 22
+#define hall_bl 26
+#define hall_br 28
 
-bool test_flag = 1;
+bool test_flag = 0;
+int count = 0;
 
 void setup() {
+  Serial.begin(9600);
+  
   pinMode(m1, OUTPUT);
   pinMode(m2, OUTPUT);
   pinMode(m3, OUTPUT);
@@ -21,51 +24,83 @@ void setup() {
   pinMode(hall_bl, INPUT_PULLUP);
   pinMode(hall_br, INPUT_PULLUP);
 
+  digitalWrite(m1, HIGH);
+  digitalWrite(m2, HIGH);
+  digitalWrite(m3, HIGH);
+  digitalWrite(m4, HIGH);
+
   test();
 
   initialize();
+  Serial.println("Initialized");
   delay(2000);
 
-  digitalWrite(m1, LOW);
-  digitalWrite(m2, LOW);
-  digitalWrite(m3, LOW);
-  digitalWrite(m4, LOW);
+  drive();
 }
 
 void loop() {
-  //Lol idhar kuch bhi nahi hai
+  Serial.println("Count : " + String(count));
+  if(!digitalRead(hall_fl)){
+    while(!digitalRead(hall_fl));
+    count++;
+  }
+    
+
+  if(count % 4 == 1){
+    stop();
+    initialize();
+    delay(1000);
+    count++;
+  }
+
+  else
+    drive();
 }
 
 
 
 void initialize() {
-  digitalWrite(m1, LOW);
-  while (!digitalRead(hall_fl));
+  while(digitalRead(hall_fr))
+  {
+    test();
+    digitalWrite(m4, LOW);
+  }
+  digitalWrite(m4, HIGH);
+
+  while(digitalRead(hall_bl))
+  {
+    test();
+    digitalWrite(m2, LOW);
+  }
+  digitalWrite(m2, HIGH);
+  
+  while(digitalRead(hall_fl))
+  {
+    test();
+    digitalWrite(m3, LOW);
+  }
+  digitalWrite(m3, HIGH);
+  
+  while(digitalRead(hall_br))
+  {
+    test();
+    digitalWrite(m1, LOW);
+  }
   digitalWrite(m1, HIGH);
 
-  digitalWrite(m2, LOW);
-  while (!digitalRead(hall_fr));
-  digitalWrite(m2, HIGH);
-
-  digitalWrite(m3, LOW);
-  while (!digitalRead(hall_bl));
-  digitalWrite(m3, HIGH);
-
-  digitalWrite(m4, LOW);
-  while (!digitalRead(hall_br));
-  digitalWrite(m4, HIGH);
+  Serial.println("Initialize done");
 }
 
 void test() {
-  if (test_flag)
-    while (!Serial.available()) {
-      Serial.print("Front Left : " + String(digitalRead(hall_fl)));
+//  if (test_flag)
+//    while (!Serial.available()) {
       Serial.print("Front Right : " + String(digitalRead(hall_fr)));
-      Serial.print("Back Left : " + String(digitalRead(hall_bl)));
-      Serial.println("Back Right : " + String(digitalRead(hall_br)));
-    }
-
-   else
-    return;
+      Serial.print(" Back Left : " + String(digitalRead(hall_bl)));
+      Serial.print(" Front Left : " + String(digitalRead(hall_fl)));
+      Serial.println(" Back Right : " + String(digitalRead(hall_br)));
+//    }
+//
+//   else
+//    return;
 }
 
