05566881135eb3c33cd984ab499acda8b87f5f4d
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2019-04-03 16:15:41 +0530

    Pins Changed

--- MR2_Automatic/MR2_Automatic.ino
+++ MR2_Automatic/MR2_Automatic.ino
@@ -1,18 +1,18 @@
-const int mTLd = 8;
-const int mTLp = 9;
-const int mTRd = 6;
-const int mTRp = 7;
+const int mTLd = 10;
+const int mTLp = 11;
+const int mTRd = 8;
+const int mTRp = 9;
 const int mBLd = 3;
 const int mBLp = 4;
-const int mBRd = 22;
-const int mBRp = 2;
+const int mBRd = 5;
+const int mBRp = 6;
 
 const int gerege_dir = 11;
 const int gerege_pwm = 12;
 
-const int hall_fr = 52;
-const int hall_fl = 53;
-const int hall_br = 51;
+const int hall_fr = 22;
+const int hall_fl = 24;
+const int hall_br = 28;
 const int hall_bl = 50;
 
 const int ir_gerege = 10;
@@ -24,7 +24,9 @@ bool a1Flag = 0, b1Flag = 0, a2Flag = 0, b2Flag = 0, a3Flag = 0, b3Flag = 0, lfl
 int count_line1 = 100, turn_count = 50, sand_dune_count = 20, turn_count2 = 30, ninety_count = 40, tussock_count = 50, uphill_count = 60;
 unsigned long timer = 0;
 
-//void walk(byte tl = 250, byte tr = 250, byte bl = 250, byte br = 250);
+int hall_count = 0;
+
+void walk(byte tl = 250, byte tr = 250, byte bl = 250, byte br = 250);
 
 void setup() {
   pinMode(mTLd, OUTPUT);
@@ -38,35 +40,49 @@ void setup() {
   pinMode(hall_br, INPUT);
   pinMode(hall_bl, INPUT);
 
-  pinMode(5, OUTPUT);
   pinMode(ir_gerege, INPUT_PULLUP);
 
-  pinMode(36, OUTPUT);
-  pinMode(38, OUTPUT);
-  
+  pinMode(12, OUTPUT);
+  pinMode(7, OUTPUT);
+
+  //12, 7 ground
+
   digitalWrite(mTLd, !HIGH);
-  digitalWrite(mTRd, !HIGH);
+  digitalWrite(mTRd, HIGH);
   digitalWrite(mBLd, !HIGH);
   digitalWrite(mBRd, HIGH);
 
-  digitalWrite(36, LOW);
-  digitalWrite(38, LOW);
+  digitalWrite(12, LOW);   //Ground
+  digitalWrite(7, LOW);  //Ground
 
-  digitalWrite(5, LOW);   //Ground
-  digitalWrite(10, LOW);  //Ground
-  
   Serial.begin(9600);
-//    test();
+  //    test();
   //  while (1);
-  //  initialize();
+    initialize();
   //  delay(1500);
-  //  while(1);
-  delay(5000);
+  //  while (1);
+  delay(3000);
 }
 
 void loop() {
   //  while(!Serial.available());
-      flow();
+  //  flow();
+  walk();
+
+  while (digitalRead(hall_fr))
+    Serial.println("Waiting for hall in count   ");
+  while(!digitalRead(hall_fr));
+  hall_count++;
+  Serial.println("Count = " + String(hall_count));
+
+  if (hall_count % 4 == 0){
+    initialize();
+    stop();
+    delay(1000);
+  }
+
+    
+
   //  timer = millis();
   //  while(millis() - timer <= 1300);
   //  stop();
--- MR2_Relay/MR2_Relay.ino
+++ MR2_Relay/MR2_Relay.ino
@@ -13,7 +13,7 @@ int count = 0;
 
 void setup() {
   Serial.begin(9600);
-  
+
   pinMode(m1, OUTPUT);
   pinMode(m2, OUTPUT);
   pinMode(m3, OUTPUT);
@@ -29,24 +29,27 @@ void setup() {
   digitalWrite(m3, HIGH);
   digitalWrite(m4, HIGH);
 
-  test();
+//  test();
 
-  initialize();
+//  initialize();b
   Serial.println("Initialized");
   delay(2000);
 
-  drive();
+  digitalWrite(m4, LOW);
+  digitalWrite(m2, LOW);
+
+  while(1);
 }
 
 void loop() {
   Serial.println("Count : " + String(count));
-  if(!digitalRead(hall_fl)){
-    while(!digitalRead(hall_fl));
+  if (!digitalRead(hall_fl)) {
+    while (!digitalRead(hall_fl));
     count++;
   }
-    
 
-  if(count % 4 == 1){
+
+  if (count % 4 == 0) {
     stop();
     initialize();
     delay(1000);
@@ -58,33 +61,49 @@ void loop() {
 }
 
 
+void initialize2(){
+  while(digitalRead(hall_fr) || digitalRead(hall_bl)){
+    if(digitalRead(hall_fr))  digitalWrite(m4, LOW);
+    else  digitalWrite(m4, HIGH);
+
+    if(digitalRead(hall_bl))  digitalWrite(m2, LOW);
+    else  digitalWrite(m2, HIGH);
+  }
+
+  while(digitalRead(hall_fl) || digitalRead(hall_br)){
+    if(digitalRead(hall_fl))  digitalWrite(m3, LOW);
+    else   digitalWrite(m3, HIGH);
+
+    if(digitalRead(hall_br))  digitalWrite(m1, LOW);
+    else  digitalWrite(m1, HIGH);
+  }
+}
 
 void initialize() {
-  while(digitalRead(hall_fr))
-  {
+  digitalWrite(m4, LOW);
+  while (digitalRead(hall_fr)){
     test();
-    digitalWrite(m4, LOW);
   }
   digitalWrite(m4, HIGH);
 
-  while(digitalRead(hall_bl))
-  {
+
+  digitalWrite(m2, LOW);
+  while (digitalRead(hall_bl)){
     test();
-    digitalWrite(m2, LOW);
   }
   digitalWrite(m2, HIGH);
+
   
-  while(digitalRead(hall_fl))
-  {
+  digitalWrite(m3, LOW);
+  while (digitalRead(hall_fl)){
     test();
-    digitalWrite(m3, LOW);
   }
   digitalWrite(m3, HIGH);
-  
-  while(digitalRead(hall_br))
-  {
+
+
+  digitalWrite(m1, LOW);
+  while (digitalRead(hall_br)){
     test();
-    digitalWrite(m1, LOW);
   }
   digitalWrite(m1, HIGH);
 
@@ -92,15 +111,8 @@ void initialize() {
 }
 
 void test() {
-//  if (test_flag)
-//    while (!Serial.available()) {
-      Serial.print("Front Right : " + String(digitalRead(hall_fr)));
-      Serial.print(" Back Left : " + String(digitalRead(hall_bl)));
-      Serial.print(" Front Left : " + String(digitalRead(hall_fl)));
-      Serial.println(" Back Right : " + String(digitalRead(hall_br)));
-//    }
-//
-//   else
-//    return;
+  Serial.print("Front Right : " + String(digitalRead(hall_fr)));
+  Serial.print(" Back Left : " + String(digitalRead(hall_bl)));
+  Serial.print(" Front Left : " + String(digitalRead(hall_fl)));
+  Serial.println(" Back Right : " + String(digitalRead(hall_br)));
 }
-
