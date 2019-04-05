5f7acea31aeaf3103e2254721df4cfc413aa5dd8
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2019-04-05 07:07:19 +0530

    no change

--- MR2_Automatic/MR2_Automatic.ino
+++ MR2_Automatic/MR2_Automatic.ino
@@ -10,16 +10,17 @@ const int mBRp = 6;
 const int gerege_dir = 11;
 const int gerege_pwm = 12;
 
-const int hall_fr = 22;
-const int hall_fl = 24;
-const int hall_br = 28;
-const int hall_bl = 26;
+const int hall_fr = 19;
+const int hall_fl = 18;
+const int hall_br = 21;
+const int hall_bl = 20;
 
 const int ir_gerege = 10;
 
 bool state = HIGH;
 byte reading = 0;
-volatile int counter1 = 0, counter2 = 0, counter3 = 0;
+volatile int tlcnt = 0, trcnt = 0, blcnt = 0, brcnt = 0;
+int counter1 = 0, counter2 = 0, counter3 = 0;
 bool a1Flag = 0, b1Flag = 0, a2Flag = 0, b2Flag = 0, a3Flag = 0, b3Flag = 0, lflag = 1;
 int count_line1 = 100, turn_count = 50, sand_dune_count = 20, turn_count2 = 30, ninety_count = 40, tussock_count = 50, uphill_count = 60;
 unsigned long timer = 0;
@@ -55,6 +56,11 @@ void setup() {
   digitalWrite(7, LOW);    //Ground
   digitalWrite(48, LOW);   //Ground
 
+  attachInterrupt(digitalPinToInterrupt(hall_fl), hall_fl_func, FALLING);
+  attachInterrupt(digitalPinToInterrupt(hall_fr), hall_fr_func, FALLING);
+  attachInterrupt(digitalPinToInterrupt(hall_bl), hall_bl_func, FALLING);
+  attachInterrupt(digitalPinToInterrupt(hall_br), hall_br_func, FALLING);
+
   Serial.begin(9600);
   //    test();
   //  while (1);
@@ -66,20 +72,22 @@ void setup() {
 
 void loop() {
   //  while(!Serial.available());
-  //  flow();
   walk();
-
-  delay(100);
-  while (digitalRead(hall_fl))
-    Serial.println("Waiting for hall in count");
-  hall_count++;
-  Serial.println("Count = " + String(hall_count));
 
   if (hall_count % 3 == 0) {
     initialize();
     stop();
     delay(1000);
   }
-  else
-    while (!digitalRead(hall_fl));
+  
+
+//  delay(100);
+//  while (digitalRead(hall_fl))
+//    Serial.println("Waiting for hall in count");
+//  hall_count++;
+//  Serial.println("Count = " + String(hall_count));
+//
+  
+//  else
+//    while (!digitalRead(hall_fl));
 }
---
+++ MR2_Automatic/interrupts.ino
@@ -0,0 +1,15 @@
+void hall_fl_func() {
+  tlcnt++;
+}
+
+void hall_fr_func() {
+  trcnt++;
+}
+
+void hall_bl_func() {
+  blcnt++;
+}
+
+void hall_br_func() {
+  brcnt++;
+}
