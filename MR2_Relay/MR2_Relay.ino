b69d02c4d3216a11b32c9e3884a342a935191cc7
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2019-04-11 19:30:09 +0530

    Chal na be bhosadike

--- MR2_Relay/MR2_Relay.ino
+++ MR2_Relay/MR2_Relay.ino
@@ -1,18 +1,22 @@
-#define dtl 5
-#define ptl 6
+#define dtl 8
+#define ptl 9
 
-#define pbr 9
-#define dbr 8
+#define dbr 5
+#define pbr 6
 
-#define mTL 34
-#define mTR 36
-#define mBL 40
-#define mBR 42
+
+//#define mTL 34
+#define mTR 42
+#define mBL 44
+//#define mBR 42
 
 #define hall_fl 21
 #define hall_fr 20
 #define hall_bl 19
 #define hall_br 18
+
+#define stl 150
+#define sbr 255
 
 volatile int count = 0;
 volatile unsigned long timer = 0;
@@ -21,24 +25,21 @@ volatile bool flag = 0;
 void setup() {
   Serial.begin(9600);
 
-  pinMode(mTL, OUTPUT);
+  //  pinMode(mTL, OUTPUT);
   pinMode(mTR, OUTPUT);
   pinMode(mBL, OUTPUT);
-  pinMode(mBR, OUTPUT);
+  //  pinMode(mBR, OUTPUT);
   pinMode(dbr, OUTPUT);
   pinMode(dtl, OUTPUT);
 
   pinMode(7, OUTPUT);
   pinMode(10, OUTPUT);
-  
+
   digitalWrite(7, LOW);
   digitalWrite(10, LOW);
 
-  digitalWrite(dtl, LOW);
-  analogWrite(ptl, 150);
-
   digitalWrite(dbr, LOW);
-  analogWrite(pbr, 150);
+  digitalWrite(dtl, HIGH);
 
   pinMode(hall_fl, INPUT_PULLUP);
   pinMode(hall_fr, INPUT_PULLUP);
@@ -46,16 +47,9 @@ void setup() {
   pinMode(hall_br, INPUT_PULLUP);
 
   Serial.println("In Setup");
-
-  //  digitalWrite(mTL, LOW);
-  //  while (1);
   stop();
 
   initialize();
-  //  while (1)
-  //  {
-  //    hall();
-  //  }
   delay(1000);
 }
 
@@ -73,7 +67,7 @@ void hall() {
   Serial.print("Front Left : " + String(digitalRead(hall_fl)));
   Serial.print(" Front Right : " + String(digitalRead(hall_fr)));
   Serial.print(" Back Left : " + String(digitalRead(hall_bl)));
-  Serial.println(" Back Right : " + String(!digitalRead(hall_br)));
+  Serial.println(" Back Right : " + String(digitalRead(hall_br)));
 }
 
 void isr() {
--- MR2_Relay/Motors.ino
+++ MR2_Relay/Motors.ino
@@ -1,40 +1,37 @@
 void walk() {
-  digitalWrite(mTL, LOW);
   digitalWrite(mTR, LOW);
   digitalWrite(mBL, LOW);
-  digitalWrite(mBR, LOW);
+  analogWrite(ptl, stl);
+  analogWrite(pbr, sbr);
 }
 
 void stop() {
-  digitalWrite(mTL, HIGH);
   digitalWrite(mTR, HIGH);
   digitalWrite(mBL, HIGH);
-  digitalWrite(mBR, HIGH);
+  analogWrite(ptl, 0);
+  analogWrite(pbr, 0);
 }
 
 void initialize() {
-    Serial.println("Set to hone de re ");
+  Serial.println("Set to hone de re");
 
-    while (digitalRead(hall_bl) != 0){
-      digitalWrite(mBL, LOW);
-      hall();
-    }
-    digitalWrite(mBL, HIGH);
+  while (digitalRead(hall_bl) != 0) {
+    digitalWrite(mBL, LOW);
+  }
+  digitalWrite(mBL, HIGH);
 
-    
-    while (digitalRead(hall_fr) != 0){
-      digitalWrite(mTR, LOW);
-     
-    }
-    digitalWrite(mTR, HIGH);
+  while (digitalRead(hall_fr) != 0) {
+    digitalWrite(mTR, LOW);
+  }
+  digitalWrite(mTR, HIGH);
 
-    while (digitalRead(hall_br) != 0){
-      digitalWrite(mBR, LOW);
-    }
-    digitalWrite(mBR, HIGH);
+  while (digitalRead(hall_br) != 0) {
+    analogWrite(pbr, sbr);
+  }
+  analogWrite(pbr, 0);
 
-    while (digitalRead(hall_fl) != 0){
-      digitalWrite(mTL, LOW);
-    }
-    digitalWrite(mTL, HIGH);
+  while (digitalRead(hall_fl) != 0) {
+    analogWrite(ptl, stl);
+  }
+  analogWrite(ptl, 0);
 }
